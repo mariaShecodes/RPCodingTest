06c572c371cb1af84c540f47469e7c716141c8bc
Maria Pelaez <maria.shecodes@gmail.com>
2020-05-10 23:24:35 +0200

    final version

--- src/components/CatsCard.js
+++ src/components/CatsCard.js
@@ -1,65 +1,55 @@
-import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, OverlayTrigger } from 'react-bootstrap';
 
 import '../style/card_style.css';
 
-class CatsCard extends Component {
-  state = {
-    picture_loading: true,
-    picture_error: null,
-    picture_data: undefined,
-    fact_loading: true,
-    fact_error: null,
-    fact_data: {}
-  }
+const CatsCard = (props) => {
 
-  componentDidMount() {
-    this.fetchPicture();
-    this.fetchFact();
-  }
+  const [ picture, setPicture ] = useState('')
+  const [ fact, setFact ] = useState('')
 
-  fetchPicture = async () => {
-    try {
-      this.setState({ picture_loading: true, picture_error: null });
-      
-      const data = await fetch('https://cataas.com/cat', {cache: 'no-cache'})
-      console.log('SOy DATA', data.body);
-      this.setState({ picture_loading: false, picture_data: data.body });
-    } catch (error) {
-      console.log(error)
-      // this.setState({ picture_loading: false, picture_error: error });
-    }
-  }
+  useEffect(() => {
+    fetch(`https://cataas.com/cat?s=${props.index}`, { cache: 'reload' })
+      .then(response => response)
+      .then(data => setPicture(data))
+  }, []);
+  
+  useEffect(() => {
+    fetch('https://cat-fact.herokuapp.com/facts/random', { cache: 'no-cache' })
+      .then(response => response.json())
+      .then(data => setFact(data.text))
+  }, []);
 
-  fetchFact = async () => {
-    this.setState({ fact_loading: true, fact_error: null })
-    
-    try {
-      const data = await fetch('https://cat-fact.herokuapp.com/facts/random');
-      this.setState({ fact_loading: false, fact_data: data });
-    } catch (error) {
-      this.setState({ fact_loading: false, fact_error: error });
-    }
-  }
-
-  render() {
-    console.log('DATA IS', this.state.fact_data);
-    const { picture_data } = this.state;
-    return (
-      <>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img src={picture_data} className="photo" />
-          <Card.Body>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-          </Card.Body>
-        </Card>
-      </>
-    )
-  }
+  return (
+    <Card style={{ width: '20rem' }} className={`div${props.index}`}>
+      {picture.url === undefined ? (
+        <img src="./spinner.gif" alt="Loading" className='spinner'  />
+      ) : (
+        <Card.Img src={picture.url} alt='Random cat picture' className='photo'/>
+      )}
+      <Card.Body className='card-body'>
+        <Card.Text className='text'>{fact}</Card.Text>
+        { (fact !== '' && fact.length > 101) && (
+          <>
+            <OverlayTrigger
+              delay={{ show: 100, hide: 100 }}
+              placement='bottom'
+              overlay={
+                <div className='overlay-text'>
+                  {fact}
+                </div>
+              }
+            >
+              <Card.Link href='#' className='button'>Read More</Card.Link>
+            </OverlayTrigger>
+          </>
+        )}
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default CatsCard;
+
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,36 @@ import '../style/home_style.css';
 import CatsCard from './CatsCard'
 
 class Home extends Component {
+  state = {
+    cards: [
+      {id: 0},{id: 1},{id: 2},{id: 3},
+    ],
+    loadingCard: true 
+  }
+
+  reload = () => {
+    this.setState({
+      ...this.state,
+      loadingCard: !this.state.loadingCard,
+    });
+  }
 
   render() {
+    const { cards } = this.state;
     return (
       <>
-        <div className="background-home">
-          <div className="container-home">
+        <div className='background-home'>
+          <div className='container-home'>
             <h1>Random Cat Stuff</h1>
-            <CatsCard />
+            <div className='container-cards'>
+              {cards.map(elm => (
+                <CatsCard index={elm.id} key={Math.random()}/>
+              ))
+            }
+            </div>
+            <div className='shuffle-button' onClick={this.reload}>
+              <p>Shuffle</p>
+            </div>
           </div>
         </div>
       </>
